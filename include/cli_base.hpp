8a800c238e17ddcb1e132cf589c551e0a7aae2d5
John Harwell <john.r.harwell@gmail.com>
2016-11-08 18:46:27 -0600

    Add basic boost command line interface class

---
+++ include/cli_base.hpp
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Name            : cli_base.hpp
+ * Project         : rcppsw
+ * Module          : cli
+ * Description     : Header file for abstract class CLI.
+ * Creation Date   : Fri Jun 19 16:10:50 2015
+ * Original Author : jharwell
+ *
+ ******************************************************************************/
+
+#ifndef _CLI_BASE_HPP_
+#define _CLI_BASE_HPP_
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include <boost/program_options.hpp>
+#include "altypes.h"
+
+/*******************************************************************************
+ * Namespace Definitions
+ ******************************************************************************/
+namespace bpo = boost::program_options;
+
+/*******************************************************************************
+ * Class Definitions
+ ******************************************************************************/
+class cli_base {
+ public:
+  cli_base(
+      const std::string& mnemonic = "");
+  virtual ~cli_base() {}
+
+  int parse(int argc,char **argv);
+  virtual status_t validate(void) { return OK; }
+  void print(void);
+  bpo::variables_map vm(void) { return vm_; }
+  bpo::options_description desc(void) { return desc_; }
+  std::string prog_name(void) { return prog_name_; }
+
+ private:
+  bpo::variables_map vm_;
+  bpo::options_description desc_;
+  std::string prog_name_;
+  std::string output_dir_base_;
+};
+
+#endif /*  _CLI_BASE_HPP_  */
--- include/threadable.hpp
+++ include/threadable.hpp
@@ -22,43 +22,43 @@
  ******************************************************************************/
 namespace rcppsw {
 
-    class threadable
-    {
-    public:
-        /* data members */
+class threadable
+{
+ public:
+  /* data members */
 
-        /* member functions */
-        threadable(void) : thread_run(false),
-                           internal_thread() {}
-        virtual ~threadable(void) {}
+  /* member functions */
+  threadable(void) : thread_run(false),
+                     internal_thread() {}
+  virtual ~threadable(void) {}
 
-        virtual void start(
-            int core = -1);
+  virtual void start(
+      int core = -1);
 
-        virtual void term(void)
-            {
-                thread_run = false;
-                internal_thread.join();
-            } /* phenf::term() */
-        bool terminated(void) { return (false == thread_run); }
-        virtual void join(void) { internal_thread.join(); }
-        virtual void thread_main(void) = 0;
-        status_t thread_core_lock(
-            pthread_t thread,
-            int core);
+  virtual void term(void)
+  {
+    thread_run = false;
+    internal_thread.join();
+  }
+  bool terminated(void) { return (false == thread_run); }
+  virtual void join(void) { internal_thread.join(); }
+  virtual void thread_main(void) = 0;
+  status_t thread_core_lock(
+      pthread_t thread,
+      int core);
 
-    protected:
-        pthread_t thread_handle(void) { return internal_thread.native_handle(); }
+ protected:
+  pthread_t thread_handle(void) { return internal_thread.native_handle(); }
 
-    private:
-        /* data members */
-        bool thread_run;
-        std::thread internal_thread;
+ private:
+  /* data members */
+  bool thread_run;
+  std::thread internal_thread;
 
-        /* non-member functions */
-        static void entry_point(void* this_p) { threadable *pt = (threadable*)this_p; pt->thread_main(); }
-        /* operators */
-    };
+  /* non-member functions */
+  static void entry_point(void* this_p) { threadable *pt = (threadable*)this_p; pt->thread_main(); }
+  /* operators */
+};
 
 } /* namespace rcppsw */
 
