ce10050998303dbd0df2ce4b33102d6eed83aa83
John Harwell <john.r.harwell@gmail.com>
2017-07-25 11:56:34 -0500

    Add expression class for use in ARGoS

---
+++ include/rcppsw/math/expression.hpp
@@ -0,0 +1,86 @@
+/**
+ * @file expression.hpp
+ *
+ * @copyright 2017 John Harwell, All rights reserved.
+ *
+ * This file is part of RCPPSW.
+ *
+ * RCPPSW is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * RCPPSW is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * RCPPSW.  If not, see <http://www.gnu.org/licenses/
+ */
+
+#ifndef INCLUDE_RCPPSW_MATH_EXPRESSION_HPP_
+#define INCLUDE_RCPPSW_MATH_EXPRESSION_HPP_
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include <vector>
+#include <cstddef>
+#include "rcsw/common/common.h"
+#include "rcsw/common/fpc.h"
+
+/*******************************************************************************
+ * Namespaces
+ ******************************************************************************/
+namespace rcppsw {
+
+/*******************************************************************************
+ * Class Definitions
+ ******************************************************************************/
+/**
+ * @brief A base class for easy swapping/manipulating of mathematical expressions.
+ */
+template<typename T>
+class expression {
+ public:
+  explicit expression(size_t n_vars) :
+      vars_(n_vars) {}
+
+  /**
+   * @brief Get the last value calculated
+   */
+  T last(void) { return last_; }
+
+  /**
+   * @brief Calculate the result of the expression over the variables.
+   *
+   * @return The new result.
+   */
+  virtual T calculate(void) = 0;
+
+  /**
+   * @brief Update the variable values in the expression and calculate the new
+   * value over the updated variables.
+   *
+   * @param vars The new variables.
+   *
+   * @return \ref status_t.
+   */
+  status_t update(const std::vector<T>& vars) {
+    FPC_CHECK(ERROR, vars.size() == vars_.size);
+    vars_ = vars;
+    last_ = calculate();
+    return OK;
+  }
+
+ protected:
+  std::vector<T>& vars(void) const { return vars_; }
+
+ private:
+  T last_;
+  std::vector<T> vars_;
+};
+
+} /* namespace rcppsw */
+
+#endif /* INCLUDE_RCPPSW_MATH_EXPRESSION_HPP_ */
