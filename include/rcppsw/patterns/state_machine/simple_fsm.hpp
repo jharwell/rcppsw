b3767bfbe342488276b8c9f6cc2a209c92df5d3c
John Harwell <john.r.harwell@gmail.com>
2017-10-04 21:43:16 -0500

    Address #26: Move functionality into base_fsm from other FSMs

--- include/rcppsw/patterns/state_machine/simple_fsm.hpp
+++ include/rcppsw/patterns/state_machine/simple_fsm.hpp
@@ -24,13 +24,6 @@
 /*******************************************************************************
  * Includes
  ******************************************************************************/
-#include <stdio.h>
-#include <typeinfo>
-#include <cstddef>
-#include <mutex>
-#include "rcppsw/common/common.hpp"
-#include "rcppsw/patterns/state_machine/event.hpp"
-#include "rcppsw/patterns/state_machine/state.hpp"
 #include "rcppsw/patterns/state_machine/base_fsm.hpp"
 
 /*******************************************************************************
@@ -39,42 +32,9 @@
 NS_START(rcppsw, patterns, state_machine);
 
 /*******************************************************************************
- * Class Definitions
+ * Type Definitions
  ******************************************************************************/
-/**
- * @brief simple_fsm implements a software-based state machine.
- */
-class simple_fsm: public base_fsm {
- public:
-  /**
-   * @param max_states The maximum number of state machine states.
-   * @param initial_state Initial state machine state.
-   */
-  simple_fsm(const std::shared_ptr<common::er_server>& server,
-             uint8_t max_states,
-             uint8_t initial_state = 0);
-
-  virtual ~simple_fsm(void) {}
-
-  virtual uint8_t current_state(void) const { return m_current_state; }
-  virtual uint8_t max_states(void) const { return mc_max_states; }
-  virtual uint8_t previous_state(void) const { return m_previous_state; }
-  virtual uint8_t last_state(void) const { return m_last_state; }
-
- protected:
-  virtual uint8_t next_state(void) const { return m_next_state; }
-  virtual uint8_t initial_state(void) const { return m_initial_state; }
-  virtual void next_state(uint8_t next_state) { m_next_state = next_state; }
-  virtual void update_state(uint8_t new_state);
-
- private:
-  const uint8_t     mc_max_states;      /// The maximum # of fsm states.
-  uint8_t           m_current_state;    /// The current state machine state.
-  uint8_t           m_next_state;        /// The next state to transition to.
-  uint8_t           m_initial_state;
-  uint8_t           m_previous_state;
-  uint8_t           m_last_state;
-};
+typedef base_fsm simple_fsm;
 
 NS_END(state_machine, patterns, rcppsw);
 
