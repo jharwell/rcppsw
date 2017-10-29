c161a3eb6aa0ba16e3e0780b7c0b5a72b68802fd
John Harwell <john.r.harwell@gmail.com>
2017-10-28 22:00:36 -0500

    Little more separation

---
+++ include/rcppsw/patterns/state_machine/state_map_row.hpp
@@ -0,0 +1,54 @@
+/**
+ * @file state_map_row.hpp
+ *
+ * @copyright 2017 John Harwell, All rights reserved.
+ *
+ * This file is part of RCPPSW.
+ *
+ * RCPPSW is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * RCPPSW is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * RCPPSW.  If not, see <http://www.gnu.org/licenses/
+ */
+
+#ifndef INCLUDE_RCPPSW_PATTERNS_STATE_MACHINE_STATE_MAP_ROW_HPP_
+#define INCLUDE_RCPPSW_PATTERNS_STATE_MACHINE_STATE_MAP_ROW_HPP_
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include "rcppsw/common/common.hpp"
+#include "rcppsw/patterns/state_machine/state.hpp"
+
+/*******************************************************************************
+ * Namespaces
+ ******************************************************************************/
+NS_START(rcppsw, patterns, state_machine);
+
+/*******************************************************************************
+ * Class Definitions
+ ******************************************************************************/
+/**
+ * @brief A structure to hold a single row within the state map.
+ */
+class state_map_row {
+ public:
+  explicit state_map_row(rcppsw::patterns::state_machine::state* state) :
+      m_state(state) {}
+
+  rcppsw::patterns::state_machine::state* state(void) const { return m_state; }
+
+ private:
+  rcppsw::patterns::state_machine::state* m_state;
+};
+
+NS_END(state_machine, patterns, rcppsw);
+
+#endif /* INCLUDE_RCPPSW_PATTERNS_STATE_MACHINE_STATE_MAP_ROW_HPP_ */
