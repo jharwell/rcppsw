dd7b2e7cce1776e36647ce6a383afdc3ca1f513e
John Harwell <john.r.harwell@gmail.com>
2017-11-26 14:11:16 -0600

    Address #55: Add xml param parsing stuff from fordyca
    +semver: feature

---
+++ include/rcppsw/common/xml_param_parser.hpp
@@ -0,0 +1,53 @@
+/**
+ * @file xml_param_parser.hpp
+ *
+ * @copyright 2017 John Harwell, All rights reserved.
+ *
+ * This file is part of RCPPSW.
+ *
+ * RCPPSW is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * RCPPSW is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * RCPPSW.  If not, see <http://www.gnu.org/licenses/
+ */
+
+#ifndef INCLUDE_RCPPSW_COMMON_XML_PARAM_PARSER_HPP_
+#define INCLUDE_RCPPSW_COMMON_XML_PARAM_PARSER_HPP_
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include <iosfwd>
+#include "rcppsw/common/common.hpp"
+
+/*******************************************************************************
+ * Namespaces
+ ******************************************************************************/
+namespace ticpp { class Element; }
+
+NS_START(rcppsw, common);
+struct base_params;
+
+/*******************************************************************************
+ * Class Definitions
+ ******************************************************************************/
+class xml_param_parser {
+ public:
+  xml_param_parser(void) {}
+  virtual ~xml_param_parser(void) {}
+
+  virtual void parse(ticpp::Element&) = 0;
+  virtual void show(std::ostream& stream) = 0;
+  virtual const struct base_params* get_results(void) = 0;
+};
+
+NS_END(common, rcppsw);
+
+#endif /* INCLUDE_RCPPSW_COMMON_XML_PARAM_PARSER_HPP_ */
