e024033b6889f10201f0195a16091b84b9ea2ff0
John Harwell <john.r.harwell@gmail.com>
2018-04-23 15:32:35 -0500

    bugfix(#111): Fix abort probability calculation
    - Not tested, but now reflects the smooth surface I graphed in matlab.
    
    +semver: bugfix

--- src/task_allocation/src/abort_probability.cpp
+++ src/task_allocation/src/abort_probability.cpp
@@ -34,19 +34,18 @@ NS_START(rcppsw, task_allocation);
  ******************************************************************************/
 double abort_probability::calc(double exec_time,
                                const time_estimate& whole_task) {
-  double omega_ta;
-
   if (!(whole_task.last_result() > 0)) {
     return set_result(kNO_EST_ABORT_PROB);
   }
 
-  if (exec_time / whole_task.last_result() <= m_offset) {
-    omega_ta = m_reactivity * (m_offset - exec_time / whole_task.last_result());
-    return set_result(1.0 - 1.0 / (1 + std::exp(-omega_ta)));
-  } else {
-    omega_ta = m_reactivity * (exec_time / whole_task.last_result() - m_offset);
-    return set_result(1.0 / (1 + std::exp(-omega_ta)));
-  }
+  double theta = m_reactivity * (m_offset - exec_time /
+                                 whole_task.last_result());
+  return set_result(1.0 / (1 + std::exp(theta)));
 } /* calc() */
+
+double abort_probability::operator()(double exec_time,
+                                     const time_estimate& whole_task) {
+  return calc(exec_time, whole_task);
+} /* operator() */
 
 NS_END(task_allocation, rcppsw);
