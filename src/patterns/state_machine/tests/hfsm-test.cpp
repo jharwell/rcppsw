6ef7772ccb65156ddf2e237a1d33d11a50699f15
John Harwell <john.r.harwell@gmail.com>
2018-06-06 10:22:23 -0500

    bugfix(#124): Fix unit tests to compile, pass for CI prep
    +semver: bugfix

--- src/patterns/state_machine/tests/hfsm-test.cpp
+++ src/patterns/state_machine/tests/hfsm-test.cpp
@@ -26,12 +26,13 @@
 #include <catch.hpp>
 #include <memory>
 #include "rcppsw/patterns/state_machine/hfsm.hpp"
+#include "rcppsw/er/server.hpp"
 
 /*******************************************************************************
  * Namespaces
  ******************************************************************************/
-using namespace rcppsw::common;
 namespace fsm = rcppsw::patterns::state_machine;
+namespace er = rcppsw::er;
 
 /*******************************************************************************
  * Test Classes
@@ -40,15 +41,15 @@ class test_fsm : public fsm::hfsm {
  public:
   enum states { STATE1, STATE2, STATE3, STATE4, STATE5, STATE6, ST_MAX_STATES };
 
-  explicit test_fsm(std::shared_ptr<er_server> server)
-      : hfsm(server), m_initial_state(0) {}
+  explicit test_fsm(const std::shared_ptr<er::server>& server)
+      : fsm::hfsm(server, ST_MAX_STATES), m_initial_state(0),
+        s1(hfsm::top_state()),
+        s2(hfsm::top_state()),
+        s3(hfsm::top_state()),
+        s4(hfsm::top_state()),
+        s5(hfsm::top_state()),
+        s6(hfsm::top_state()) {}
 
-  HFSM_STATE_DECLARE(test_fsm, hfsm, top_state, s1, fsm::no_event_data);
-  HFSM_STATE_DECLARE(test_fsm, hfsm, top_state, s2, fsm::no_event_data);
-  HFSM_STATE_DECLARE(test_fsm, hfsm, top_state, s3, fsm::no_event_data);
-  HFSM_STATE_DECLARE(test_fsm, hfsm, top_state, s4, fsm::no_event_data);
-  HFSM_STATE_DECLARE(test_fsm, hfsm, top_state, s5, fsm::no_event_data);
-  HFSM_STATE_DECLARE(test_fsm, hfsm, top_state, s6, fsm::no_event_data);
   void event1(void) {
     FSM_DEFINE_TRANSITION_MAP(kMAP){STATE2,
                                     STATE3,
@@ -70,15 +71,6 @@ class test_fsm : public fsm::hfsm {
                    rcppsw::make_unique<fsm::event_data>(2));
   }
 
-  FSM_DEFINE_STATE_MAP_ACCESSOR(state_map) {
-    FSM_DEFINE_STATE_MAP(state_map, kMAP){FSM_STATE_MAP_ENTRY(&s1),
-                                          FSM_STATE_MAP_ENTRY(&s2),
-                                          FSM_STATE_MAP_ENTRY(&s3),
-                                          FSM_STATE_MAP_ENTRY(&s4),
-                                          FSM_STATE_MAP_ENTRY(&s5),
-                                          FSM_STATE_MAP_ENTRY(&s6)};
-    return &kMAP[0];
-  }
   uint8_t current_state(void) const { return m_current_state; }
   uint8_t max_states(void) const { return ST_MAX_STATES; }
   uint8_t next_state(void) const { return m_next_state; }
@@ -87,36 +79,53 @@ class test_fsm : public fsm::hfsm {
   uint8_t initial_state(void) const { return m_initial_state; }
 
  private:
+  HFSM_STATE_DECLARE_ND(test_fsm, s1);
+  HFSM_STATE_DECLARE_ND(test_fsm, s2);
+  HFSM_STATE_DECLARE_ND(test_fsm, s3);
+  HFSM_STATE_DECLARE_ND(test_fsm, s4);
+  HFSM_STATE_DECLARE_ND(test_fsm, s5);
+  HFSM_STATE_DECLARE_ND(test_fsm, s6);
+
+  FSM_DEFINE_STATE_MAP_ACCESSOR(state_map, index) {
+    FSM_DEFINE_STATE_MAP(state_map, kMAP){FSM_STATE_MAP_ENTRY(&s1),
+          FSM_STATE_MAP_ENTRY(&s2),
+          FSM_STATE_MAP_ENTRY(&s3),
+          FSM_STATE_MAP_ENTRY(&s4),
+          FSM_STATE_MAP_ENTRY(&s5),
+          FSM_STATE_MAP_ENTRY(&s6)};
+    return &kMAP[index];
+  }
+
   uint8_t m_current_state;
   uint8_t m_initial_state;
   uint8_t m_next_state;
 };
 
-HFSM_STATE_DEFINE(test_fsm, s1, fsm::event_data) {
+HFSM_STATE_DEFINE_ND(test_fsm, s1) {
   printf("Executing state1\n");
   return fsm::event_signal::HANDLED;
 }
-HFSM_STATE_DEFINE(test_fsm, s2, fsm::event_data) {
+HFSM_STATE_DEFINE_ND(test_fsm, s2) {
   printf("Executing state2\n");
   return fsm::event_signal::HANDLED;
 }
 
-HFSM_STATE_DEFINE(test_fsm, s3, fsm::event_data) {
+HFSM_STATE_DEFINE_ND(test_fsm, s3) {
   printf("Executing state3\n");
   return fsm::event_signal::HANDLED;
 }
 
-HFSM_STATE_DEFINE(test_fsm, s4, fsm::event_data) {
+HFSM_STATE_DEFINE_ND(test_fsm, s4) {
   printf("Executing state4\n");
   internal_event(STATE5, rcppsw::make_unique<fsm::event_data>(4));
   return fsm::event_signal::HANDLED;
 }
-HFSM_STATE_DEFINE(test_fsm, s5, fsm::event_data) {
+HFSM_STATE_DEFINE_ND(test_fsm, s5) {
   printf("Executing state5\n");
   return fsm::event_signal::HANDLED;
 }
 
-HFSM_STATE_DEFINE(test_fsm, s6, fsm::event_data) {
+HFSM_STATE_DEFINE_ND(test_fsm, s6) {
   printf("Executing state6\n");
   return fsm::event_signal::HANDLED;
 }
@@ -125,7 +134,7 @@ HFSM_STATE_DEFINE(test_fsm, s6, fsm::event_data) {
  * Test Functions
  ******************************************************************************/
 CATCH_TEST_CASE("sanity-test", "[hfsm]") {
-  test_fsm fsm(std::make_shared<er_server>("logfile"));
+  test_fsm fsm(er::g_server);
   fsm.init();
   CATCH_REQUIRE(fsm.current_state() == test_fsm::STATE1);
   fsm.event1();
@@ -137,7 +146,7 @@ CATCH_TEST_CASE("sanity-test", "[hfsm]") {
 }
 
 CATCH_TEST_CASE("event-test", "[hfsm]") {
-  test_fsm fsm(std::make_shared<er_server>("logfile"));
+  test_fsm fsm(er::g_server);
   fsm.init();
   CATCH_REQUIRE(fsm.current_state() == test_fsm::STATE1);
   fsm.event2();
