aea9244a73a98de4492a9b1fc26520be07169a9e
toekneechin777 <chen4714@umn.edu>
2019-02-05 13:54:53 -0600

    Added change battery level wrapper

--- include/rcppsw/robotics/hal/sensors/battery_sensor.hpp
+++ include/rcppsw/robotics/hal/sensors/battery_sensor.hpp
@@ -1,7 +1,7 @@
 /**
  * @file battery_sensor.hpp
  *
- * @copyright 2018 The Nimer Inc., All rights reserved.
+ * @copyright 2019 The Nimer Inc./Anthony Chen/John Harwell, All rights reserved.
  * This file is part of RCPPSW.
  *
  * RCPPSW is free software: you can redistribute it and/or modify it under the
@@ -17,88 +17,96 @@
  * RCPPSW.  If not, see <http://www.gnu.org/licenses/
  */
 
- #ifndef INCLUDE_RCPPSW_ROBOTICS_HAL_SENSORS_BATTERY_SENSOR_HPP_
- #define INCLUDE_RCPPSW_ROBOTICS_HAL_SENSORS_BATTERY_SENSOR_HPP_
+#ifndef INCLUDE_RCPPSW_ROBOTICS_HAL_SENSORS_BATTERY_SENSOR_HPP_
+#define INCLUDE_RCPPSW_ROBOTICS_HAL_SENSORS_BATTERY_SENSOR_HPP_
 
- /******************************************************************************
-  * Includes
-  ******************************************************************************/
- #include <vector>
- #include "rcppsw/common/common.hpp"
- #include "rcppsw/robotics/hal/hal.hpp"
+/******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include <vector>
+#include "rcppsw/common/common.hpp"
+#include "rcppsw/robotics/hal/hal.hpp"
 
- #if HAL_CONFIG == HAL_CONFIG_ARGOS_FOOTBOT
- #include <argos3/plugins/robots/generic/control_interface/ci_battery_sensor.h>
- #else
- #error "Selected hardware has no battery sensor!"
- #endif /* HAL_CONFIG */
+#if HAL_CONFIG == HAL_CONFIG_ARGOS_FOOTBOT
+#include <argos3/plugins/robots/generic/control_interface/ci_battery_sensor.h>
+#else
+#error "Selected hardware has no battery sensor!"
+#endif /* HAL_CONFIG */
 
- /******************************************************************************
-  * Namespaces
-  ******************************************************************************
-*/
-NS_START(rcppsw, robotics, hal, sensors);
+/******************************************************************************
+ * Namespaces/Decls
+ ******************************************************************************/
+NS_START(rcppsw, robotics, hal, sensors, detail);
 
- /******************************************************************************
-  * Class Definitions
-  ******************************************************************************/
-  /**
-   * @class battery_sensor
-   * @ingroup robotics hal
-   *
-   * @brief Battery sensor wrapper for the following supported robots:
-   *
-   * - ARGoS footbot
-   */
+/*******************************************************************************
+ * Templates
+ ******************************************************************************/
+template<typename Sensor>
+using is_argos_battery_sensor = std::is_same<Sensor,
+                                             argos::CCI_BatterySensor>;
+
+NS_END(detail);
+
+/******************************************************************************
+ * Class Definitions
+ ******************************************************************************/
+/**
+ * @class battery_sensor
+ * @ingroup robotics hal
+ *
+ * @brief Battery sensor wrapper for the following supported robots:
+ *
+ * - ARGoS footbot
+ */
 template <typename T>
 class _battery_sensor {
-public:
+ public:
   /**
-   * @brief A battery sensor reading (value, angle) pair.
+   * @brief A battery sensor reading.
    *
-   * The first argument is the amount of battery left (1 meaning 100% and 0
-   * being 0%), and the second argument is the amount of time left for the robot
-   * (there is an "infinite" time option to essentially diable the battery for
-   * the simulation).
+   * The first argument is the amount of battery left as a percent between 0 and
+   * 1, and the second argument is the amount of time left for the robot.
+   *
+   * Note that these are different representations of the same essential
+   * information: how much energy the robot current has left.
    */
-   typedef struct reading {
-     double availible_charge;
-     double time_left;
+  struct reading {
+    double availible_charge;
+    double time_left;
 
-     reading(double _availible_charge, double _time_left)
-         : availible_charge(_availible_charge),
-           time_left(_time_left) {}
-   } reading;
-
-   template<typename U = T>
-   explicit _battery_sensor(typename std::enable_if_t<std::is_same<U,
-                            argos::CCI_BatterySensor>::value,
-                            argos::CCI_BatterySensor> * sensor)
-       : m_sensor(sensor) {}
-
-    /**
-    * @brief Get the current battery sensor readings for
-    * the footbot robot.
-    * @return A vector of \ref reading.
-    */
-    template <typename U = T>
-    typename std::enable_if_t<std::is_same<U, argos::CCI_BatterySensor>::value, reading>
-    readings(void) const {
-      argos::CCI_BatterySensor::SReading temp = m_sensor->GetReading();
-      reading ret(temp.AvailableCharge, temp.TimeLeft);
-
-
-      return ret;
-    }
-
-   private:
-    T* m_sensor;
+    reading(double _availible_charge, double _time_left)
+        : availible_charge(_availible_charge),
+          time_left(_time_left) {}
   };
 
-  #if HAL_CONFIG == HAL_CONFIG_ARGOS_FOOTBOT
-  using battery_sensor = _battery_sensor<argos::CCI_BatterySensor>;
-  #endif /* HAL_CONFIG */
+  explicit _battery_sensor(T * const sensor) : m_sensor(sensor) {}
 
-  NS_END(sensors, hal, robotics, rcppsw);
+  /**
+   * @brief Get the current battery sensor readings for the footbot robot.
+   * @return A vector of \ref reading.
+   */
+  template <typename U = T,
+            RCPPSW_SFINAE_REQUIRE(detail::is_argos_battery_sensor<U>::value)>
+  struct reading readings(void) const {
+    argos::CCI_BatterySensor::SReading temp = m_sensor->GetReading();
+    reading ret(temp.AvailableCharge, temp.TimeLeft);
+    return ret;
+  }
 
-  #endif /* INCLUDE_RCPPSW_ROBOTICS_HAL_SENSORS_BATTERY_SENSOR_HPP_ */
+  template <typename U = T,
+            RCPPSW_SFINAE_REQUIRE(detail::is_argos_battery_sensor<U>::value)>
+  void setCharge(double newEnergyLevel) {
+    SetAvailableCharge(newEnergyLevel);
+  }
+
+ private:
+  T* const m_sensor;
+};
+
+#if HAL_CONFIG == HAL_CONFIG_ARGOS_FOOTBOT
+using battery_sensor = _battery_sensor<argos::CCI_BatterySensor>;
+#endif /* HAL_CONFIG */
+
+NS_END(sensors, hal, robotics, rcppsw);
+
+#endif /* INCLUDE_RCPPSW_ROBOTICS_HAL_SENSORS_BATTERY_SENSOR_HPP_ */
