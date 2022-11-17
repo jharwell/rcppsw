771576e35afc09fa8eed2838bcab54a6d66ebebf
John Harwell <john.r.harwell@gmail.com>
2022-11-17 12:13:50 -0600

    Switch to MIT license

---
+++ tests/spatial-euclidean_dist-utest.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file spatial-euclidean_dist-utest.cpp
+ *
+ * @copyright 2021 John Harwell, All rights reserved.
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_PREFIX_ALL
+#include "catch.hpp"
+#include "rcppsw/spatial/euclidean_dist.hpp"
+
+/*******************************************************************************
+ * Namespaces
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Test Cases
+ ******************************************************************************/
+CATCH_TEST_CASE("Sanity", "[rspatial::euclidean_dist]") {
+  rspatial::euclidean_dist d1(0);
+  CATCH_REQUIRE(0 == d1.v());
+
+  auto d2 = rspatial::euclidean_dist::make(-3);
+  CATCH_REQUIRE(3 == d2.v());
+}
+
+CATCH_TEST_CASE("Operations", "[rspatial::euclidean_dist]") {
+  rspatial::euclidean_dist d1(2);
+  d1 += rspatial::euclidean_dist(3);
+
+
+  rspatial::euclidean_dist d2(2);
+  rspatial::euclidean_dist d3(2);
+
+  CATCH_REQUIRE(d1 >= d3);
+  CATCH_REQUIRE(d2 - 1.0 <= 2.0);
+
+  CATCH_REQUIRE(d2 >= d2);
+  CATCH_REQUIRE(d2 - 1 < d2);
+}
